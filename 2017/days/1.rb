95e83e20134583f7721b19c6985c55e1410aad2d
Dan Petker <dan.petker@vidyard.com>
2017-12-11 11:11:24 -0500

    Day 1, part 1

--- 2017/days/1.rb
+++ 2017/days/1.rb
@@ -1,1 +1,30 @@
-puts "Hello, adventofcode!"
+File.open("../inputs/1.txt").each do |line|
+  prev_digit = -1
+  digit = -1
+  sum = 0
+
+  if line[0] == "#"
+    next
+  end
+
+  line.split("").each do |char|
+    if char == "\n"
+      next
+    end
+
+    digit = char.to_i
+
+    if digit == prev_digit
+      sum += digit
+    end
+
+    prev_digit = digit
+  end
+
+  # check if last digit matches first
+  if digit == line[0].to_i
+    sum += digit
+  end
+
+  puts "CAPTCHA part 1 decode complete! Your answer is: #{sum}."
+end
