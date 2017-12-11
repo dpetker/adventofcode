95544248327227545908a4b8cbc41a60e82898b2
Dan Petker <dan.petker@vidyard.com>
2017-12-11 11:42:09 -0500

    Day 1, part 2 (and cleanup)

--- 2017/days/1.rb
+++ 2017/days/1.rb
@@ -1,17 +1,14 @@
+# Part 1 of the CAPTCHA decode - comparing to previous digit
 File.open("../inputs/1.txt").each do |line|
-  prev_digit = -1
-  digit = -1
-  sum = 0
-
   if line[0] == "#"
     next
   end
 
-  line.split("").each do |char|
-    if char == "\n"
-      next
-    end
+  prev_digit = -1
+  digit = -1
+  sum = 0
 
+  line.gsub("\n", "").split("").each do |char|
     digit = char.to_i
 
     if digit == prev_digit
@@ -27,4 +24,29 @@ File.open("../inputs/1.txt").each do |line|
   end
 
   puts "CAPTCHA part 1 decode complete! Your answer is: #{sum}."
+end
+
+# Part 2 of CAPTCHA decode - compare to halfway around
+File.open("../inputs/1.txt").each do |line|
+  if line[0] == "#"
+    next
+  end
+
+  line = line.gsub("\n", "")
+
+  size = line.size
+  halfway = size / 2
+  sum = 0
+
+  line.split("").each_with_index do |char, index|
+    digit = char.to_i
+    halfway_index = (index + halfway) % size
+    halfway_digit = line[halfway_index].to_i
+
+    if digit == halfway_digit
+      sum += digit
+    end
+  end
+
+  puts "CAPTCHA part 2 decode complete! Your answer is: #{sum}."
 end
