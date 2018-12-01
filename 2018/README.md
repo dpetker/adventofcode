f8660ec22a2c8344e70f1f74ecc74ac954d4a380
Dan Petker <dan.petker@vidyard.com>
2018-12-01 14:24:51 -0500

    Solution for Day 1, 2018

---
+++ 2018/README.md
@@ -0,0 +1,23 @@
+# Advent of Code - 2018
+
+Solutions for [Advent of Code 2018](https://adventofcode.com/2018).
+
+## Usage
+
+These solutions are built with Python 3 using [`pipenv`](https://pipenv.readthedocs.io/en/latest/). To get yourself set up, install `pipenv` and create a Python 3 environment with:
+
+```sh
+$ pipenv --python 3
+```
+
+Then activate the `pipenv` shell with:
+
+```sh
+$ pipenv shell
+```
+
+At which point you should be able to run a given day's solution with:
+
+```sh
+$ python src/day1.py
+```
---
+++ 2018/src/day1.py
@@ -0,0 +1,32 @@
+# Solution for Day 1 of Advent of Code 2018
+# Problem description: https://adventofcode.com/2018/day/1
+
+import os
+
+def calculateFrequency(change_list, current_freq, seen_freqs):
+  duplicate_freq = None
+
+  for number in change_list:
+    current_freq += number
+
+    if (current_freq in seen_freqs) and (duplicate_freq is None):
+      duplicate_freq = current_freq
+
+    seen_freqs.add(current_freq)
+
+  return (current_freq, seen_freqs, duplicate_freq)
+
+if __name__ == '__main__':
+  with open(os.path.join(os.path.dirname(__file__), '../input/day1.txt'), 'r') as f:
+    lines = f.readlines()
+    day1_input = [int(line.strip()) for line in lines]
+
+  current_freq, seen_freqs, duplicate_freq = calculateFrequency(day1_input, 0, set())
+  result_freq = current_freq
+
+  while duplicate_freq is None:
+    current_freq, seen_freqs, duplicate_freq = calculateFrequency(day1_input, current_freq, seen_freqs)
+
+  print(f"Resulting Frequency is: {result_freq}")
+  print(f"First duplicate Frequency is: {duplicate_freq}")
+
