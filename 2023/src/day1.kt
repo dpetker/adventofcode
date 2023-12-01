33c55f3a34cee8502e288fb8d1443a2c91eba25c
Dan Petker <dpetker@tempo.io>
2023-12-01 14:34:48 -0500

    Day 1 - part 1

--- 2023/src/day1.kt
+++ 2023/src/day1.kt
@@ -1,3 +1,21 @@
+import java.io.File
+
 fun main() {
-  println("Hello, adventofcode 2023!")
+  val lineList = mutableListOf<String>()
+
+  // The File() call is relative to the directory we're running run.sh from
+  File("./input/day1.txt").useLines { lines -> lines.forEach { lineList.add(it) } }
+
+  var part1RunningTotal = 0
+
+  lineList.forEach { line ->
+    val firstDigit = line.filter { it.isDigit() }.firstOrNull()
+    val lastDigit = line.reversed().filter { it.isDigit() }.firstOrNull()
+
+    val twoDigit = "$firstDigit$lastDigit".toInt()
+
+    part1RunningTotal += twoDigit
+  }
+
+  println("Part 1 total: $part1RunningTotal")
 }
