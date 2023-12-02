3ba4e48e99d72888ba6e0e18b167cdb25554fc4f
Dan Petker <dpetker@tempo.io>
2023-12-01 23:24:58 -0500

    Day 1 - ruby solution - converted to class

--- 2023/src/day1.rb
+++ 2023/src/day1.rb
@@ -1,47 +1,53 @@
 # day1.rb - Day 1 solution, ruby-fied!
+class Day1
+  @@number_to_digit_hash = {
+    'one' => 1,
+    'two' => 2,
+    'three' => 3,
+    'four' => 4,
+    'five' => 5,
+    'six' => 6,
+    'seven' => 7,
+    'eight' => 8,
+    'nine' => 9,
+    '1' => 1,
+    '2' => 2,
+    '3' => 3,
+    '4' => 4,
+    '5' => 5,
+    '6' => 6,
+    '7' => 7,
+    '8' => 8,
+    '9' => 9
+  }
 
-# Part 1
-part_1_running_total = 0
-File.open("./input/day1.txt").each do |line|
-  if line[0] == "#"
-    next
+  def initialize(input_lines)
+    @input_lines = input_lines
   end
 
-  all_digits = line.scan(/\d/)
-  part_1_running_total += "#{all_digits.first}#{all_digits.last}".to_i
-end
+  def part_1
+    @input_lines.sum do |line|
+      if line[0] == "#"
+        next
+      end
 
-puts "Part 1 total: #{part_1_running_total}"
-
-# Part 2
-part_2_running_total = 0
-number_to_digit_hash = {
-  'one' => 1,
-  'two' => 2,
-  'three' => 3,
-  'four' => 4,
-  'five' => 5,
-  'six' => 6,
-  'seven' => 7,
-  'eight' => 8,
-  'nine' => 9,
-  '1' => 1,
-  '2' => 2,
-  '3' => 3,
-  '4' => 4,
-  '5' => 5,
-  '6' => 6,
-  '7' => 7,
-  '8' => 8,
-  '9' => 9
-}
-File.open("./input/day1.txt").each do |line|
-  if line[0] == "#"
-    next
+      all_digits = line.scan(/\d/)
+      "#{all_digits.first}#{all_digits.last}".to_i
+    end
   end
 
-  all_digits = line.scan(/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/).flatten
-  part_2_running_total += "#{number_to_digit_hash[all_digits.first]}#{number_to_digit_hash[all_digits.last]}".to_i
+  def part_2
+    @input_lines.sum do |line|
+      if line[0] == "#"
+        next
+      end
+
+      all_digits = line.scan(/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/).flatten
+      "#{@@number_to_digit_hash[all_digits.first]}#{@@number_to_digit_hash[all_digits.last]}".to_i
+    end
+  end
 end
 
-puts "Part 2 total: #{part_2_running_total}" # Part 2 total: 54518
+day_1 = Day1.new(IO.readlines("./input/day1.txt"))
+puts "Part 1 total: #{day_1.part_1}" # Part 1 total: 54331
+puts "Part 2 total: #{day_1.part_2}" # Part 2 total: 54518
