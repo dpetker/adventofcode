e202105d62d1ebe9d9e76b6363f7cfc25af6ed61
Dan Petker <dpetker@tempo.io>
2023-12-03 21:48:01 -0500

    Unfinished go code for day 3

--- 2023/day3.go
+++ 2023/day3.go
@@ -1,11 +1,54 @@
 package main
 
-type Day3 struct{}
+import (
+	"regexp"
+)
 
+type Day3 struct {
+	engine Engine
+}
+
+type Engine struct {
+	connectedParts   map[string][]int
+	unconnectedParts []int
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+/* Day-related methods */
 func (d Day3) Part1() int {
 	return 0
 }
 
+func (d Day3) Part2() int {
+	return 0
+}
+
 func CreateDay3(lines []string) Day3 {
-	return Day3{}
+	engine := CreateEngine(lines)
+	return Day3{engine}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+/* Engine-related methods */
+func CreateEngine(lines []string) Engine {
+	e := Engine{make(map[string][]int), []int{}}
+
+	runeTest := regexp.MustCompile(`^(\.|\d)$`)
+	for rowNum, line := range lines {
+		for colNum, val := range line {
+			valAsStr := string(val)
+			if !runeTest.MatchString(valAsStr) {
+				_, ok := e.connectedParts[valAsStr]
+				if !ok {
+					e.connectedParts[valAsStr] = []int{}
+				}
+
+				// TODO: search around valAsStr for numbers
+			}
+		}
+	}
+
+	return e
 }
