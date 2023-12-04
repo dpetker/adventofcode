5b5acb6288b73a622715c29bf06dd92262a0b0e8
Dan Petker <dpetker@tempo.io>
2023-12-03 22:34:10 -0500

    Day 3 - ruby - boilerplate

---
+++ 2023/day3.rb
@@ -0,0 +1,17 @@
+class Day3
+  def initialize(use_sample_data=false)
+    data_path = use_sample_data ? './input/day3_sample.txt' : './input/day3.txt'
+    @data = IO.readlines(data_path)
+  end
+
+  def part_1
+  end
+
+  def part_2
+  end
+end
+
+class Engine
+  def initialize(raw_schematic)
+  end
+end
---
+++ 2023/spec/day3_spec.rb
@@ -0,0 +1,12 @@
+require_relative "../day3"
+
+describe Day3 do
+  describe "(sample data)" do
+    describe "part_1" do
+      it "should come up with 4361" do
+        d3 = Day3.new(use_sample_data=true)
+        expect(d3.part_1).to eq(4361)
+      end
+    end
+  end
+end
