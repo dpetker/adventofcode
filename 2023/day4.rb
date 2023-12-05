c2361c1a154aec8295fc8952fb55f8a99e39fb31
Dan Petker <dpetker@tempo.io>
2023-12-05 16:56:00 -0500

    Day 4 - part 1 solution

--- 2023/day4.rb
+++ 2023/day4.rb
@@ -2,11 +2,39 @@ class Day4
   def initialize(use_sample_data=false)
     data_path = use_sample_data ? './input/day4_sample.txt' : './input/day4.txt'
     @data = IO.readlines(data_path)
+    @cards = @data.map {|line| Card.new(line)}
   end
 
   def part_1
+    @cards.sum {|card| card.points}
   end
 
   def part_2
   end
+end
+
+class Card
+  attr_reader :id
+
+  def initialize(data_line)
+    tokens = data_line.split(':')
+    @id = tokens[0].sub('Card', '').strip.to_i
+    winning_str, have_str = tokens[1].split('|')
+
+    @winning_vals = winning_str.split(' ').map {|e| e.to_i}
+    @have_vals = have_str.split(' ').map {|e| e.to_i}
+  end
+
+  def points
+    match_count = num_matches
+    return 0 if match_count == 0
+
+    2**(match_count - 1)
+  end
+
+  def num_matches
+    @have_vals.intersection(@winning_vals).length
+  end
+
+  private :num_matches
 end
--- 2023/spec/day4_spec.rb
+++ 2023/spec/day4_spec.rb
@@ -10,23 +10,23 @@ describe Day4 do
     end
 
     describe "part_2" do
-      it "should come up with nil" do
+      it "should come up with 30" do
         d4 = Day4.new(use_sample_data=true)
-        expect(d4.part_2).to eq(nil)
+        expect(d4.part_2).to eq(30)
       end
     end
   end
 
   describe "(actual data)" do
     describe "part_1" do
-      it "should come up with nil" do
+      it "should come up with 24733" do
         d4 = Day4.new()
-        expect(d4.part_1).to eq(nil)
+        expect(d4.part_1).to eq(24733)
       end
     end
 
     describe "part_2" do
-      it "should come up with 80253814" do
+      it "should come up with nil" do
         d4 = Day4.new()
         expect(d4.part_2).to eq(nil)
       end
